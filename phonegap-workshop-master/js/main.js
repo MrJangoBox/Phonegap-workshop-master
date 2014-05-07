fed5d6b329bed0b7c547e483d23b638caea94ff2
MrJangoBox <d3t0ur@hotmail.com>
2014-05-07 14:59:29 -0400

    05_Part5
    - HandleBar template addition

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,16 +1,25 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' +                   e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
+
+        
+//        Inline html previous
+//        console.log('findByName');
+//        this.store.findByName($('.search-key').val(), function(employees) {
+//            var l = employees.length;
+//            var e;
+//            $('.employee-list').empty();
+//            for (var i=0; i<l; i++) {
+//                e = employees[i];
+//                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' +                   e.lastName + '</a></li>');
+//            }
+//        });
+        
     },
 
     showAlert: function (message, title) {
@@ -22,18 +31,26 @@ var app = {
     },
     
     renderHomeView: function() {
-        var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'</ul>" +
-            "</div>"
-        $('body').html(html);
+        
+//      Inline html
+//        var html =
+//            "<div class='header'><h1>Home</h1></div>" +
+//            "<div class='search-view'>" +
+//            "<input class='search-key'/>" +
+//            "<ul class='employee-list'</ul>" +
+//            "</div>"
+        
+        $('body').html(this.homeTpl());
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
     
     initialize: function() {
         var self = this;
+        
+        // Compile the two templates
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
         this.store = new MemoryStore(function () {
             self.renderHomeView();
             self.showAlert('Store Initialized', 'info');
