a9288f09dafb0fdf1357dc7c73094559aeb6605d
MrJangoBox <d3t0ur@hotmail.com>
2014-05-08 16:49:40 -0400

    Merge branch 'Tutorial-Parts'

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,17 +1,27 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' +                   e.lastName + '</a></li>');
-            }
-        });
-    },
+//    Moved to HomeVew class Completely
+//    findByName: function() {
+//
+//        var self = this;
+//        this.store.findByName($('.search-key').val(), function(employees) {
+//            $('.employee-list').html(self.employeeLiTpl(employees));
+//        });
+
+        
+//        Inline html previous
+//        console.log('findByName');
+//        this.store.findByName($('.search-key').val(), function(employees) {
+//            var l = employees.length;
+//            var e;
+//            $('.employee-list').empty();
+//            for (var i=0; i<l; i++) {
+//                e = employees[i];
+//                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' +                   e.lastName + '</a></li>');
+//            }
+//        });
+        
+//    },
 
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -21,25 +31,45 @@ var app = {
         }
     },
     
-    renderHomeView: function() {
-        var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'</ul>" +
-            "</div>"
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
+//    Moved to HomeVew class Completely
+//    renderHomeView: function() {
+        
+//      Inline html
+//        var html =
+//            "<div class='header'><h1>Home</h1></div>" +
+//            "<div class='search-view'>" +
+//            "<input class='search-key'/>" +
+//            "<ul class='employee-list'</ul>" +
+//            "</div>"
+        
+//        $('body').html(this.homeTpl());
+//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//    },
     
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function () {
-            self.renderHomeView();
-            self.showAlert('Store Initialized', 'info');
+        self.showAlert('Store Initialized', 'info');
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
-//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+    
+//    initialize: function() {
+//        var self = this;
+//        
+//        Compile the two templates, privious
+//        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+//        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        
+//        this.store = new MemoryStore(function () {
+//            Previous
+//            self.renderHomeView();
+//            self.showAlert('Store Initialized', 'info');
+            
+//            $('body').html(new HomeView(self.store).render().el);
+//        });
+//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//    }
 };
 
 app.initialize();
